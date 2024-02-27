49cb8a18663d070f27eb1d14a323164f2ca37138
kristiyanmm <160527887+kristiyanmm@users.noreply.github.com>
2024-02-27 15:38:36 +0200

    Delete Portfolio directory

--- Portfolio/main.js
+++
@@ -1,7 +0,0 @@
-var typed= new Typed(".text", {
-    strings: ["Frontend Developer", "Website Designer", "Graphic Designer", "C# Developer", "Content Creator"],
-    typeSpeed: 100,
-    backSpeed: 100,
-    backDelay: 1000,
-    loop:true
-});
